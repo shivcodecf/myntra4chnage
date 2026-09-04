c02d5b98887c25b135eb1f39959c279ec23eb47f
Shivam Yadav <shivamforcoding69@gmail.com>
2026-09-05 02:09:16 +0530

    fix cors issue

--- frontend/my-app/lib/api.ts
+++ frontend/my-app/lib/api.ts
@@ -1,10 +1,17 @@
 import axios from "axios";
 
+
+console.log("api",process.env.NEXT_PUBLIC_API_URL)
+
 const api = axios.create({
-  baseURL: process.env.API_URL,
+  
+
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
+
+  
 });
 
 export default api;
